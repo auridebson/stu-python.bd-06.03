99d39bc5150f8e194d70cfaeb75479eae9248845
Auridebson Lima <auridebson@vutus.com>
2024-03-06 18:24:08 -0300

    Update

--- main.py
+++ main.py
@@ -11,10 +11,28 @@ cursor.execute("""
 def addUsuario(nome, curso, email, senha):
     pass
 
+def criar_tabela_alunos():
+    # Conectar ao banco de dados (o arquivo será criado se não existir)
+    conexao = sqlite3.connect('exemplo.db')
+
+    # Criar um cursor para interagir com o banco de dados
+    cursor = conexao.cursor()
+
+    # Executar comando SQL para criar a tabela
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS alunos (
+            id INTEGER PRIMARY KEY,
+            nome TEXT,
+            curso TEXT,
+            email TEXT
+        )
+    ''')
+
+
+
+
+
 conn.commit()
 cursor.close()
 conn.close()
 
-
-
-print("Aplicativo de acesso ao BD")
