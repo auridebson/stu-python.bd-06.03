7bcc78730550c165b7f8e928e4bbed1bdf482515
Auridebson Lima <auridebson@vutus.com>
2024-03-06 18:17:27 -0300

    Update

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ conn = sqlite3.connect('escola')
 
 cursor = conn.cursor()
 
-
-
 cursor.execute("""
-    SELECT * FROM alunos
-
+    SELECT * FROM escola
 """)
 
+def addUsuario(nome, curso, email, senha):
+    pass
+
+conn.commit()
+cursor.close()
+conn.close()
+
+
+
+print("Aplicativo de acesso ao BD")
